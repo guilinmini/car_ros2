a6cd07d2f2f91b13abb3efc014a0270599e7ef71
guilinmini <guilin9488a@gmail.com>
2026-03-06 17:54:26 +0800

    Add laser and camera sensors

--- wheeltec_robot_display/launch/mini_4wd_robot_gazebo.launch.py
+++ wheeltec_robot_display/launch/mini_4wd_robot_gazebo.launch.py
@@ -75,7 +75,12 @@ def generate_launch_description():
     bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
-        arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'],
+        arguments=[
+            '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
+            '/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan',
+            '/camera/image_raw@sensor_msgs/msg/Image@gz.msgs.Image',
+            '/camera/camera_info@sensor_msgs/msg/CameraInfo@gz.msgs.CameraInfo',
+        ],
         output='screen'
     )
 
